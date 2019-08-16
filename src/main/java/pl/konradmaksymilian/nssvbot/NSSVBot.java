b7cfa962e59c551efe71b2e2308eb00ab82718c2
maksymilianK <kondziugeniusz@gmail.com>
2019-08-16 11:44:37 +0200

    Project initialization
    Generate the project using Gradle.

---
+++ src/main/java/pl/konradmaksymilian/nssvbot/NSSVBot.java
@@ -0,0 +1,13 @@
+package pl.konradmaksymilian.nssvbot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NSSVBot {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(NSSVBot.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info("NSSVBot has been successfully initialized!");
+	}
+}
